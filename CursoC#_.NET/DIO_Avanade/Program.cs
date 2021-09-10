b5879dae70e6748082fecdb7a484cf95f831f85b
smcco94 <sergiomcastro94@gmail.com>
2021-09-10 19:30:21 -0300

    DioC#

---
+++ CursoC#_.NET/DIO_Avanade/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DIO_Avanade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 5;
+
+            for(int i = 0; i < number; i++)
+            {
+                Console.WriteLine($"Bem vindo ao .NET {i}");
+            }
+        }
+    }
+}
